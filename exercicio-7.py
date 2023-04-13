d47995d8c7929b9a7593f02d58f8d821358bc7f6
barbara carvalho de souza <barbara.carvsouza07@gmail.com>
2023-04-13 11:31:28 -0300

    Faça um Programa que leia um vetor de 5 números inteiros, mostre a soma, a multiplicação e os números.

---
+++ exercicio-7.py
@@ -0,0 +1,9 @@
+numeros= [1, 2, 3, 4, 5]
+soma = 0
+resultado = numeros[0]
+print (numeros)
+for i in numeros:
+    soma = soma + numeros[i-1]
+    resultado = resultado * numeros[i-1] 
+print (soma)
+print (resultado)
