66e57f006f2b8b83e9da412c8e2f67ed02447142
barbara carvalho de souza <barbara.carvsouza07@gmail.com>
2023-04-13 15:32:37 -0300

    Faça um Programa que peça a idade e a altura de 5 pessoas, armazene cada informação no seu respectivo vetor. Imprima a idade e a altura na ordem inversa a ordem lida.

--- exercicio-8.py
+++ exercicio-8.py
@@ -1,7 +1,15 @@
-idade = []
-altura = []
+idades = []
+tempVar = []
+resultado = ""
 for i in range(0,5):
-    print("insira a idade:")
-    altura = [input()]
-    print("insira a altura")
-    idade.append(altura)
+    print("Insira a idade:")
+    tempVar = input()
+    print("Insira a altura:")
+    tempVar = [tempVar, input()]
+    idades.append(tempVar)
+    tempVar = []
+contador = len(idades)
+for i in range(0,contador):
+    resultado = f"{resultado} {idades[contador-1]}"
+    contador -= 1
+print(resultado)
